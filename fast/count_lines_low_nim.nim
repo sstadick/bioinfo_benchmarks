6e68849f36e1708b7384aba5ec1b6bbb692f102f
Seth Stadick <sstadick@gmail.com>
2019-05-13 06:37:58 -0700

    faster low level version in nim

---
+++ fast/count_lines_low_nim.nim
@@ -0,0 +1,29 @@
+import strutils, sequtils
+
+
+
+proc main() =
+  var count = 0
+  let bufferLen = 12_000
+  var buffer = newString(bufferLen)
+  var colIndex = 0
+  while true:
+    var bytesRead = stdin.readChars(buffer, 0, bufferLen)
+    if bytesRead == 0:
+      break
+    for idx in 0 ..< bytesRead:
+      case buffer[idx]:
+        of '\t':
+          colIndex = 0
+        of '\n':
+          colIndex = 0
+        else:
+          inc(colIndex)
+      if colIndex == 1 or colIndex == 2:
+        if buffer[idx].toLowerAscii() == 'b':
+          if buffer[idx+1].toLowerAscii() == 'c':
+            inc(count)
+
+  echo count
+
+main()
